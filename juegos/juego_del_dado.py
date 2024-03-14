31e6f670cbea7a4b23814d7b8ba4e191b30be4a1
felipedlm <163411352+felipedlm@users.noreply.github.com>
2024-03-14 12:34:27 -0300

    Update juego_del_dado.py

--- juegos/juego_del_dado.py
+++ juegos/juego_del_dado.py
@@ -1,14 +1,35 @@
 def juego_del_dado():
+    """
+    Esta función tiene que pedirle al usuario que aprete enter para que lance un dado.
+    Esto genera un número al azar que se le suma a la puntuación del usuario.
+    Después el computador también tiene que lanzar un dado.
+    El primero en sumar 30 puntos gana.
+    """
     import random 
-    print("debes apretar enter para jugar")
-    puntaje_usuario=0
-    puntaje_computador=0
-    while puntaje_usuario !=30 or puntaje_computador!=30:
-        numero=random.randit(1,6)
-        puntaje_usuario+=numero
-        numero2=random.randit(1,6)
-        puntaje_computador+=numero2
-    if puntaje_usuario==30:
-        print("El usuario gana el juego")
+    enter =input("Debes apretar enter para jugar:")
+    if enter == "":
+        puntaje_usuario=0
+        puntaje_computador=0
+        ronda=1
+        while puntaje_usuario <30 and puntaje_computador<30:
+            
+            numero=random.randint(1,6)
+            
+            puntaje_usuario+=numero
+
+            numero2=random.randint(1,6)
+            puntaje_computador+=numero2
+
+            print("Ronda",ronda,"Usuario:", puntaje_usuario)
+            print("Ronda", ronda, "Computador:", puntaje_computador)
+
+            ronda+=1
+            
+        if puntaje_usuario>=30:
+            print("El usuario gana el juego")
+        else:
+            print("Gano el computador")
     else:
-        print("Gano el computador")
+        print("No apretaste enter")
+
+juego_del_dado()
